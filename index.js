e9da9db0db87187fec70b732d0d16deb01fff891
Anchal1412 <singhaj8130@gmail.com>
2026-03-12 17:00:02 +0530

    added comment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.write("Hello from Node.js server");
   res.end();
 });
-
+console.log('hi')
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
